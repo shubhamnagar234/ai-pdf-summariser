2901982662af8979251bce06a583417dd616caf8
Shubham Nagar <shubhamnagar234@gmail.com>
2026-02-15 00:14:44 +0530

    Header and Navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from "next";
-import { Source_Sans_3 as FontSans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Source_Sans_3 as FontSans } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/common/header';
+import Footer from '@/components/common/footer';
 
 const fontSans = FontSans({
-  variable: "--font-sans",
-  subsets: ["latin"],
-  weight: ["200", "300","400", "500", "600", "700", "800", "900"],
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Sommaire - AI-Powered PDF Summarization",
-  description: "Save time and effort with AI-powered PDF summarization",
+  title: 'Sommaire - AI-Powered PDF Summarization',
+  description: 'Save time and effort with AI-powered PDF summarization',
 };
 
 export default function RootLayout({
@@ -20,10 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} antialiased`}>
+        <div className="relative flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
