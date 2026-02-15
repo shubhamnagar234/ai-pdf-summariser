d7ea3df6c769c861619b922f2169bef140e40dc4
Shubham Nagar <shubhamnagar234@gmail.com>
2026-02-16 00:34:20 +0530

    Clerk Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,17 @@ import { Source_Sans_3 as FontSans } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const fontSans = FontSans({
   variable: '--font-sans',
   subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
 });
 
 export const metadata: Metadata = {
-  title: 'Sommaire - AI-Powered PDF Summarization',
-  description: 'Save time and effort with AI-powered PDF summarization',
+  title: 'Sommaire',
+  description: 'Sommaire is an App to summarize PDFs',
 };
 
 export default function RootLayout({
@@ -21,14 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fontSans.variable} antialiased`}>
-        <div className="relative flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${fontSans.variable} font-sans antialiased`}>
+          <div className="relative flex min-h-screen flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
